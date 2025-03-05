ab3bdc8c88dc9fd17aab0cfedc427ef1d15543bf
r.karabash <r.karabash@tinkoff.ru>
2025-03-05 18:19:17 +0300

    Fix

--- src/main/kotlin/ru/school57/todolist/controlleradvice/ExceptionHandler.kt
+++ src/main/kotlin/ru/school57/todolist/controlleradvice/ExceptionHandler.kt
@@ -1,11 +1,13 @@
 package ru.school57.todolist.controlleradvice
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import ru.school57.todolist.dto.ErrorResponse
 import ru.school57.todolist.exception.BadRequestException
 
+@Hidden
 @RestControllerAdvice
 class ExceptionHandler {
 
